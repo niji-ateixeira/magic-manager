1c8348ffcd4ff9aa9e2140ee06058871dd0aaf61
Teixeira <teixeira.alexandre.dev.info@gmail.com>
2018-07-09 22:22:11 +0200

    chore(add jss):

--- src/js/App.test.js
+++ src/js/App.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App.js';
 
-jest.mock('../logo.svg', () => 'logo');
-
 test('renders without crashing', () => {
   const component = renderer.create(
     <App />,
