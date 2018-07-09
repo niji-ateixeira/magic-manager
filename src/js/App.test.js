1e7f382be230b1576bce2e7fb2e6da47786335b3
Teixeira <teixeira.alexandre.dev.info@gmail.com>
2018-07-09 21:57:04 +0200

    chore(add eslint):

--- src/js/App.test.js
+++ src/js/App.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import App from './App.js';
 import renderer from 'react-test-renderer';
+import App from './App.js';
 
-jest.mock('./logo.svg', ()=>'logo');
+jest.mock('../logo.svg', () => 'logo');
 
 test('renders without crashing', () => {
   const component = renderer.create(
-    <App/>
+    <App />,
   );
 
-  let tree = component.toJSON();
+  const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
